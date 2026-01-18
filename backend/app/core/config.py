a406da67e7a5aa4016173dfd139aca56c0287b58
tresor-del <tresoradzanto7@gmail.com>
2026-01-18 11:56:28 +0000

    Update application configuration and startup scripts

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,3 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRETE_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,20 @@
-from fastapi import FastAPI
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request
 
 from app.db.database import Base, engine
+from app.routers.auth import router as auth_router
+
+templates = Jinja2Templates(directory="app/templates")
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="app/templates/static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+async def accueil(request: Request):
+        return templates.TemplateResponse("layout.html", {"request": request, "titre": "Enrollix"})
+    
+app.include_router(auth_router)
